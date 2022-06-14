29d304664f26c60a6f2604300ff7cb70e1ddd756
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-06-14 15:07:30 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,17 @@
 import React from "react";
-import user from "../data/user";
+// import user from "../data/user";
 import Links from "./Links";
 
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
